a0e2ec6bf1bc8c8fec8f52cce9e76ee627cc719a
OlenevKirill <kk.olenev@mail.ru>
2024-12-19 09:52:44 +0300

    Update task1.c

--- 3824Б1ФИ3/22 Оленев Кирилл Александрович/task1.c
+++ 3824Б1ФИ3/22 Оленев Кирилл Александрович/task1.c
@@ -1,19 +1,18 @@
 // Задача 1. Дан непустой массив A, содержащий n натуральных чисел.
 // Вернуть 1, если его элементы образуют арифметическую прогрессию,
 // иначе вернуть 0.
-int task1(unsigned A[], size_t n) 
-{
-    if (n <= 2) {
+int task1(unsigned A[], size_t n) {
+    if (n <= 1) { 
         return 1;
     }
-    int diff = (int)A[1] - (int)A[0];
-    for (size_t i = 2; i < n; ++i) 
-    {
-        if ((int)A[i] - (int)A[i - 1] != diff) 
-        {
-            return 0; 
+
+    int diff = A[1] - A[0]; 
+
+    for (size_t i = 2; i < n; i++) {
+        if (A[i] - A[i - 1] != diff) {
+            return 0;
         }
     }
 
-    return 1; 
+    return 1;
 }
