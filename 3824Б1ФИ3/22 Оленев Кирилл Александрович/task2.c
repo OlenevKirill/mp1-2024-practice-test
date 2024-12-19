3c6b5306baedb99c0c103f9b52dcc7c900d84a07
OlenevKirill <kk.olenev@mail.ru>
2024-12-19 09:50:49 +0300

    Update task2.c

--- 3824Б1ФИ3/22 Оленев Кирилл Александрович/task2.c
+++ 3824Б1ФИ3/22 Оленев Кирилл Александрович/task2.c
@@ -3,13 +3,41 @@
 // расстояние |A[k1] – A[k2]| между которыми минимально.
 // Если таких пар в массиве несколько, вернуть пару с наименьшими индексами.
 // При невозможности найти пару, вернуть {-1,-1}.
-struct Indices
-{
-  int first_index;
-  int second_index;
+#include <stdio.h>
+#include <stdlib.h>
+#include <float.h>
+#include <math.h>
+
+struct Indices {
+    int first_index;
+    int second_index;
 };
-struct Indices task2(double A[], size_t n)
-{
-  struct Indices indices = {-2, -2};
-  return indices;
+
+struct Indices task2(double A[], size_t n) {
+    struct Indices indices = {-1, -1}; 
+
+    if (n < 2) {
+        return indices; 
+    }
+
+    double min_diff = DBL_MAX; 
+    for (size_t i = 0; i < n; ++i) {
+        for (size_t j = i + 1; j < n; ++j) {
+            double diff = fabs(A[i] - A[j]);
+
+            if (diff < min_diff) {
+                min_diff = diff;
+                indices.first_index = (int)i;
+                indices.second_index = (int)j;
+            } else if (diff == min_diff) {
+                
+                if (i < indices.first_index || (i == indices.first_index && j < indices.second_index)) {
+                    indices.first_index = (int)i;
+                    indices.second_index = (int)j;
+                }
+            }
+        }
+    }
+
+    return indices;
 }
